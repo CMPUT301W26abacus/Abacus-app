5c8ce2894fc09ffb09cc973a77081bf86b149bc9
Dyna Dagman <dagman@ualberta.ca>
2026-03-13 02:25:35 -0600

    Refactor login and registration layouts: update layout references in LoginActivity and RegisterActivity, and create new XML files for login and registration pages.

--- abacus_app/app/src/main/java/com/example/abacus_app/LoginActivity.java
+++ abacus_app/app/src/main/java/com/example/abacus_app/LoginActivity.java
@@ -41,7 +41,7 @@ public class LoginActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.test_loginpage);
+        setContentView(R.layout.loginpage);
 
         mAuth = FirebaseAuth.getInstance();
 
